aadbb977a0fffaacd26b6ce4934e4f7c01ec66f7
Mike Merritt <emmjemm@gmail.com>
2012-05-03 16:33:07 -0500

    fixed demo

--- javascripts/jquery.liveFilter.js
+++ javascripts/jquery.liveFilter.js
@@ -49,64 +49,11 @@
       zebraStriping();
     }
 
-    // Cache list/table elements so we don't have to keep traversing the DOM.
-    var list = filterTarget.find(child);
-
-    // Keyup event - where the magic happens.
-    wrap.find('input[type="text"]').on("keyup", function() {
-
-      var input = $(this);
-      clearTimeout(keyDelay);
-
-      // Setting timeout for performance reasons.
-      keyDelay = setTimeout(function () { 
-
-        filter = input.val().toLowerCase();
-        var visible = 0;
-        var words = filter.split(' ');
-
-        if (filter === '' && options.hideDefault === true) {
-          list.each(function(i) {
-            $(this).hide();
-          })
-        } else {
-          // Iterate through list and show/hide the proper elements.
-          list.each(function(i) {
-            text = $(this).text().toLowerCase();
-
-            // Non consecutive filtering
-            for (var t = 0; t < words.length; t++) {
-              if (text.indexOf(words[t]) < 0) {
-                var match = false;
-                break;
-              } else {
-                var match = true;
-              }
-            }
-
-            if (match === true) {
-              visible ++;
-              $(this).show();
-            } else if (match === false) {
-              $(this).hide();
-            }
-          });
-
-          if (visible === 0) {
-            nomatches.show();
-          } else if (visible > 0) {
-            nomatches.hide();
-          }
-
-          if(options.zebra.enabled != false) {
-            zebraStriping();
-          }
-        }
-
-        clearTimeout(keyDelay);
-      }, options.delay);
-
-    });
+    // Add inputs if required
+    if (options.addInputs === true) {
+      var markup = '<input class="filter" type="text" value="" /><input class="reset" type="reset" value="Reset!" />';
+      wrap.prepend(markup);
+    }
 
     // Used to reset our text input and show all items in the filtered list
     wrap.find('input[type="reset"]').on("click", function() {
@@ -165,11 +112,64 @@
 
     }
 
-    // Add inputs if required
-    if (options.addInputs === true) {
-      var markup = '<input class="filter" type="text" value="" /><input class="reset" type="reset" value="Reset!" />';
-      wrap.prepend(markup);
-    }
+    // Cache list/table elements so we don't have to keep traversing the DOM.
+    var list = filterTarget.find(child);
+
+    // Keyup event - where the magic happens.
+    wrap.find('input[type="text"]').on("keyup", function() {
+
+      var input = $(this);
+      clearTimeout(keyDelay);
+
+      // Setting timeout for performance reasons.
+      keyDelay = setTimeout(function () { 
+
+        filter = input.val().toLowerCase();
+        var visible = 0;
+        var words = filter.split(' ');
+
+        if (filter === '' && options.hideDefault === true) {
+          list.each(function(i) {
+            $(this).hide();
+          })
+        } else {
+          // Iterate through list and show/hide the proper elements.
+          list.each(function(i) {
+            text = $(this).text().toLowerCase();
+
+            // Non consecutive filtering
+            for (var t = 0; t < words.length; t++) {
+              if (text.indexOf(words[t]) < 0) {
+                var match = false;
+                break;
+              } else {
+                var match = true;
+              }
+            }
+
+            if (match === true) {
+              visible ++;
+              $(this).show();
+            } else if (match === false) {
+              $(this).hide();
+            }
+          });
+
+          if (visible === 0) {
+            nomatches.show();
+          } else if (visible > 0) {
+            nomatches.hide();
+          }
+
+          if(options.zebra.enabled != false) {
+            zebraStriping();
+          }
+        }
+
+        clearTimeout(keyDelay);
+      }, options.delay);
+
+    });
 
     // Used for zebra striping list/table.
     function zebraStriping() {
